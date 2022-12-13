48bc03b4194aea34853a665ad6e18939a735bd18
Nusratulloh202 <nusratulloh202@gmail.com>
2022-12-13 09:58:58 +0500

    INFRA: Initialize Unit Test Project

---
+++ Sheenam.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,15 @@
+﻿//===============
+//Copyright (c) Coalition of Good-Hearted Enginers
+//Free To Use Comfort and Peace
+//===============
+using System.Reflection;
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue()=>Assert.True(true);
+    }
+}
---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿//===============
+//Copyright (c) Coalition of Good-Hearted Enginers
+//Free To Use Comfort and Peace
+//===============
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Hello Mario. Hammasi islayapti.");
+    }
+}
