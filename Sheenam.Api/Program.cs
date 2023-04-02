fc8437ade348475c57deae747e4e5eb69bac98bf
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2023-04-02 21:23:13 +0500

    Revert "INFRA:Initialize Unit Test Project"

--- Sheenam.Api/Program.cs
+++ Sheenam.Api/Program.cs
@@ -1,8 +1,4 @@
-//================================================
-//Copyright(c) Coalition of Good-Hearted Engineers
-//Free To Use To Find Comfort and Peace
-//================================================
-
+//
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
