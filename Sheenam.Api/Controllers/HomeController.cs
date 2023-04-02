dd39f2fddc850d46e201b0c2bb9e4aadff797734
Nusratulloh202 <nusratulloh202@gmail.com>
2023-04-02 20:53:07 +0500

    INFRA:Initialize API Project

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult <string> Get()=>
+            Ok("Salom Nusratulloh. Dastur ishladi");
+    }
+}
