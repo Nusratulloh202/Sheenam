b3d0877134225b2fa5a7808ec676e02185825669
Nusratulloh202 <nusratulloh202@gmail.com>
2023-04-05 13:50:16 +0500

    Data:Model & Migrate Guests

---
+++ Sheenam.Api/Broker/Storages/StorageBroker.Guests.cs
@@ -0,0 +1,14 @@
+﻿//================================================
+//Copyright(c) Coalition of Good-Hearted Engineers
+//Free To Use To Find Comfort and Peace
+//================================================
+using Microsoft.EntityFrameworkCore;
+using Sheenam.Api.Models.Foundations.Guests;
+
+namespace Sheenam.Api.Broker.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Guest> Guests { get; set; }
+    }
+}
