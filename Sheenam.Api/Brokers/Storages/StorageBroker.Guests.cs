94478e7178f356cdd79ddb69f9ba99b81f3d313f
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-02-09 00:46:37 +0500

    BROKERS:Insert Guest  #16

--- Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
+++ Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
@@ -2,13 +2,27 @@
 // Copyright (c) Coalition of Good-Hearted Engineers
 // Free To Use To Find Comfort and Peace
 //==================================================
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Sheenam.Api.Models.Foundations.Guests;
 
 namespace Sheenam.Api.Brokers.Storages
 {
-    public partial class StorageBroker
+    public partial class StorageBroker : IStorageBroker
     {
         public DbSet<Guest> Guests { get; set; }
+
+        public async ValueTask<Guest> InsertGuestAsync(Guest guest)
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<Guest> guestEntityEntry = 
+                await broker.Guests.AddAsync(guest);
+
+            await broker.SaveChangesAsync();
+
+            return guestEntityEntry.Entity;
+        }
     }
 }
