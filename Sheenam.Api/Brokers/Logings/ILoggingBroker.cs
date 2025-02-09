ffa42ef2f81634651ddbf484ba8f61b2711ca24e
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-02-09 17:52:33 +0500

    BROKERS:Implement Logging

---
+++ Sheenam.Api/Brokers/Logings/ILoggingBroker.cs
@@ -0,0 +1,14 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+using System;
+
+namespace Sheenam.Api.Brokers.Logings
+{
+    public interface ILoggingBroker
+    {
+        public void LogError(Exception exception);
+        public void LogCritical(Exception exception);
+    }
+}
---
+++ Sheenam.Api/Brokers/Logings/LoggingBroker.cs
@@ -0,0 +1,23 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace Sheenam.Api.Brokers.Logings
+{
+    public class LoggingBroker: ILoggingBroker
+    {
+        private readonly ILogger logger;
+
+        public LoggingBroker(ILogger logger) =>
+            this.logger = logger;
+
+        public void LogError(Exception exception)=>
+            this.logger.LogError(exception, exception.Message);
+        public void  LogCritical(Exception exception)=>
+            this.logger.LogCritical(exception, exception.Message);
+
+    }
+}
