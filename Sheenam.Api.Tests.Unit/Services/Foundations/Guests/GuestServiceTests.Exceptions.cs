3db055e3de039a351a991e41cd1f1944a239c728
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-03-08 01:04:57 +0500

    ShouldThrowDependencyValidationOnAddIfDublicateKeyErrorOccursAndLogItAsync->FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Exceptions.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Exceptions.cs
@@ -5,6 +5,7 @@
 using Microsoft.Data.SqlClient;
 using Moq;
 using Sheenam.Api.Models.Foundations.Guests;
+using EFxceptions.Models.Exceptions;
 using Sheenam.Api.Models.Foundations.Guests.Exceptions;
 
 namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
@@ -43,6 +44,48 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        //Dublicat key Exception uchun test
+        [Fact]
+        public async Task ShouldThrowDependencyValidationOnAddIfDublicateKeyErrorOccursAndLogItAsync()
+        {
+            //given
+            Guest sameGuest = CreateRandomGuest();
+            string Message = GetRandomMessage();
+            var duplicateKeyException = new DuplicateKeyException(Message);
+
+            var alreadyExistGuestException =
+                new AlreadyExistGuestException(duplicateKeyException);
+
+            var expectedGuestValidationException =
+                new GuestDependencyValidationException(alreadyExistGuestException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertGuestAsync(sameGuest))
+                    .ThrowsAsync(duplicateKeyException);
+
+            //when
+            ValueTask<Guest> addGuestTask =
+                this.guestService.AddGuestAsync(sameGuest);
+
+            //then
+            await Assert.ThrowsAsync<GuestDependencyValidationException>(()=>
+                addGuestTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertGuestAsync(sameGuest),
+                Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedGuestValidationException))),
+                Times.Once());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+
+
+
 
         }
     }
