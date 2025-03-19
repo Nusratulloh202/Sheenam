86f68e8c21843f603c31298efcbfde344fd0c0ec
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-03-19 22:41:29 +0500

    CONFIG:SqlException and AllIfServiceError

--- Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Exception.RetrieveAll.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Exception.RetrieveAll.cs
@@ -11,79 +11,79 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests;
 
 public partial class GuestServiceTests
 {
-    //[Fact]
-    //public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
-    //{
-    //    //given
-    //    SqlException sqlException = GetSqlException();
-    //    var failedGuestStorageException =
-    //        new FailedGuestStorageException(sqlException);
+    [Fact]
+    public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+    {
+        //given
+        SqlException sqlException = GetSqlException();
+        var failedGuestStorageException =
+            new FailedGuestStorageException(sqlException);
 
-    //    var expectedGuestDependencyException =
-    //              new GuestDependencyException(failedGuestStorageException);
-    //    this.storageBrokerMock.Setup(broker =>
-    //    broker.SelectAllGuests()).Throws(sqlException);
+        var expectedGuestDependencyException =
+                  new GuestDependencyException(failedGuestStorageException);
+        this.storageBrokerMock.Setup(broker =>
+        broker.SelectAllGuests()).Throws(sqlException);
 
-    //    //when
-    //    Action retrieveAllGuestsAction = () =>
-    //        this.guestService.RetrieveAllGuests();
-    //    GuestDependencyException actualGuestDependensyException =
-    //        Assert.Throws<GuestDependencyException>(retrieveAllGuestsAction);
+        //when
+        Action retrieveAllGuestsAction = () =>
+            this.guestService.RetrieveAllGuests();
+        GuestDependencyException actualGuestDependensyException =
+            Assert.Throws<GuestDependencyException>(retrieveAllGuestsAction);
 
-    //    //then
-    //    actualGuestDependensyException.Should()
-    //        .BeEquivalentTo(expectedGuestDependencyException);
+        //then
+        actualGuestDependensyException.Should()
+            .BeEquivalentTo(expectedGuestDependencyException);
 
-    //    this.storageBrokerMock.Verify(broker =>
-    //        broker.SelectAllGuests(),
-    //        Times.Once());
+        this.storageBrokerMock.Verify(broker =>
+            broker.SelectAllGuests(),
+            Times.Once());
 
-    //    this.loggingBrokerMock.Verify(broker =>
-    //        broker.LogCritical(It.Is(SameExceptionAs(expectedGuestDependencyException))),
-    //        Times.Once);
+        this.loggingBrokerMock.Verify(broker =>
+            broker.LogCritical(It.Is(SameExceptionAs(expectedGuestDependencyException))),
+            Times.Once);
 
-    //    this.storageBrokerMock.VerifyNoOtherCalls();
-    //    this.loggingBrokerMock.VerifyNoOtherCalls();
+        this.storageBrokerMock.VerifyNoOtherCalls();
+        this.loggingBrokerMock.VerifyNoOtherCalls();
 
 
-   // }
+    }
 
-    //[Fact]
-    //public void ShouldThrowServiceExceptionOnRetriveAllIfServiceErrorOccursAndLogIt()
-    //{
-    //    //given 
-    //    string exceptionMessage = GetRandomMessage();
-    //    var serverException = new Exception(message:exceptionMessage);
+    [Fact]
+    public void ShouldThrowServiceExceptionOnRetriveAllIfServiceErrorOccursAndLogIt()
+    {
+        //given 
+        string exceptionMessage = GetRandomMessage();
+        var serverException = new Exception(message: exceptionMessage);
 
-    //    var failedGuestServiceException =
-    //        new FailedGuestServiceException(serverException);
+        var failedGuestServiceException =
+            new FailedGuestServiceException(serverException);
 
-    //    var expectedGuestSeviceAllException=
-    //        new GuestServiceAllException(failedGuestServiceException);
-         
-    //    this.storageBrokerMock.Setup(broker =>
-    //    broker.SelectAllGuests()).Throws(serverException);
+        var expectedGuestSeviceAllException =
+            new GuestServiceAllException(failedGuestServiceException);
 
-    //    //when
-    //    Action retriveAllGuestAction=()=>
-    //        this.guestService.RetrieveAllGuests();
+        this.storageBrokerMock.Setup(broker =>
+        broker.SelectAllGuests()).Throws(serverException);
 
-    //    GuestServiceAllException actualServiceException = 
-    //        Assert.Throws<GuestServiceAllException>(retriveAllGuestAction);
+        //when
+        Action retriveAllGuestAction = () =>
+            this.guestService.RetrieveAllGuests();
 
-    //    //then
-    //    actualServiceException.Should().BeEquivalentTo(expectedGuestSeviceAllException);
+        GuestServiceAllException actualServiceException =
+            Assert.Throws<GuestServiceAllException>(retriveAllGuestAction);
 
-    //    this.storageBrokerMock.Verify(broker=>
-    //    broker.SelectAllGuests(), Times.Once());
+        //then
+        actualServiceException.Should().BeEquivalentTo(expectedGuestSeviceAllException);
 
-    //    this.loggingBrokerMock.Verify(broker=>
-    //        broker.LogError(It.Is(SameExceptionAs(expectedGuestSeviceAllException))),
-    //        Times.Once);
+        this.storageBrokerMock.Verify(broker =>
+        broker.SelectAllGuests(), Times.Once());
 
-    //    this.storageBrokerMock.VerifyNoOtherCalls();
-    //    this.loggingBrokerMock.VerifyNoOtherCalls();
-        
+        this.loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(expectedGuestSeviceAllException))),
+            Times.Once);
 
-   // }
+        this.storageBrokerMock.VerifyNoOtherCalls();
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+
+
+    }
 }
