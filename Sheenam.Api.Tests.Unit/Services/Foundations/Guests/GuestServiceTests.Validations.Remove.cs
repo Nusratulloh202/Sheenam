83acf59d6a494c0d9b599c7850d7d2cb034fd544
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-04-04 07:18:23 +0500

    ShouldThrowValidationExceptionOnRemoveIfIdIsInvaledAndLogItAsync->FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Validations.Remove.cs
@@ -0,0 +1,49 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+using Moq;
+using Sheenam.Api.Models.Foundations.Guests.Exceptions;
+using Sheenam.Api.Models.Foundations.Guests;
+using FluentAssertions;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveIfIdIsInvaledAndLogItAsync()
+        {
+            // given
+            Guid invalidId = Guid.Empty;
+            var invalidGuestIdException = new InvalidGuestException();
+            invalidGuestIdException.AddData(
+                key: nameof(Guest.Id),
+                values: "Id is required");
+            var expectedGuestValidationException = new
+                GuestValidationException(invalidGuestIdException);
+            //when
+            ValueTask<Guest> removeGuestTask =
+                this.guestService.RemoveGuestByIdAsync(invalidId);
+
+            GuestValidationException actualGuestValidationException =
+                await Assert.ThrowsAsync<GuestValidationException>(removeGuestTask.AsTask);
+            //then
+            actualGuestValidationException.Should().BeEquivalentTo(expectedGuestValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedGuestValidationException))),
+                Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuestByIdAsync(It.IsAny<Guid>()),
+                Times.Never());
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteGuestAsync(It.IsAny<Guest>()),
+                Times.Never());
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
