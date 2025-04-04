39cdc3685e3f2bb380961eef4d45064c1b1277eb
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-04-04 06:56:36 +0500

    ShouldThrowValidationExceptionOnRemoveIfIdIsInvaledAndLogItAsync->PASS

--- Sheenam.Api/Services/Foundations/Guests/GuestService.Exceptions.cs
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.Exceptions.cs
@@ -26,18 +26,18 @@ namespace Sheenam.Api.Services.Foundations.Guests
             {
                 return await returningGuestFunction();
             }
-            //catch (NullGuestException nullGuestException)
-            //{
-            //    throw CreateAndLogValidationException(nullGuestException);
-            //}
-            //catch (InvalidGuestException invalidGuestException)
-            //{
-            //    throw CreateAndLogValidationException(invalidGuestException);
-            //}
-            //catch (NotFoundGuestException notFoundGuestException)
-            //{
-            //    throw CreateAndLogValidationException(notFoundGuestException);
-            //}
+            catch (NullGuestException nullGuestException)
+            {
+                throw CreateAndLogValidationException(nullGuestException);
+            }
+            catch (InvalidGuestException invalidGuestException)
+            {
+                throw CreateAndLogValidationException(invalidGuestException);
+            }
+            catch (NotFoundGuestException notFoundGuestException)
+            {
+                throw CreateAndLogValidationException(notFoundGuestException);
+            }
             catch (SqlException sqlException)
             {
                 var failedGuestStorageException =
