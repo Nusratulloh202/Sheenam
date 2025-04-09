1c786fecc85c77ea1636965bc42d3ee2a6d8da6e
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-04-09 14:28:38 +0500

    ShoulThrowValidationExceptionOnAddIfHostIsNullAndLogItAsync->FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Validations.Add.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Validations.Add.cs
@@ -2,9 +2,48 @@
 // Copyright (c) Coalition of Good-Hearted Engineers
 // Free To Use To Find Comfort and Peace
 //==================================================
+using FluentAssertions;
+using Moq;
+using Sheenam.Api.Models.Foundations.Hosts;
+using Sheenam.Api.Models.Foundations.Hosts.Exceptions.BigExceptions;
+using Sheenam.Api.Models.Foundations.Hosts.Exceptions.SmallExceptions;
+
 namespace Sheenam.Api.Tests.Unit.Services.Foundations.Hosts
 {
     public partial class HostServiceTests
     {
+        [Fact]
+        public async Task ShoulThrowValidationExceptionOnAddIfHostIsNullAndLogItAsync()
+        {
+            // given
+            Host nullHost = null;
+            var nullHostException = new NullHostException();
+
+            var expectedHostValidationException =
+                new HostValidationException(nullHostException);
+
+            //when
+            ValueTask<Host> taskAddHost = 
+                this.hostService.AddHostAsync(nullHost);
+
+            HostValidationException actualHostValidationException =
+                await Assert.ThrowsAsync<HostValidationException>(taskAddHost.AsTask);
+
+            //then
+            actualHostValidationException.Should()
+                .BeEquivalentTo(expectedHostValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedHostValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertHostAsync(It.IsAny<Host>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+
+        }
     }
 }
--- Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.cs
@@ -2,12 +2,14 @@
 // Copyright (c) Coalition of Good-Hearted Engineers
 // Free To Use To Find Comfort and Peace
 //==================================================
+using System.Linq.Expressions;
 using Moq;
 using Sheenam.Api.Brokers.Logings;
 using Sheenam.Api.Brokers.Storages;
 using Sheenam.Api.Models.Foundations.Hosts;
 using Sheenam.Api.Services.Foundations.Hosts;
 using Tynamix.ObjectFiller;
+using Xeptions;
 
 namespace Sheenam.Api.Tests.Unit.Services.Foundations.Hosts
 {
@@ -25,6 +27,8 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Hosts
                 storageBroker: this.storageBrokerMock.Object,
                 loggingBroker: this.loggingBrokerMock.Object);
         }
+        private Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
         private static Filler<Host> CreateHostFiller(DateTimeOffset date)
         {
             var filler = new Filler<Host>();
