583295d4d52745584e09c961ceb2bf311604f100
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-04-17 09:00:49 +0500

    ShouldThrowServiceExceptionOnRetriveAllIfServerErrorOccursAndLogItAsync -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Exceptions.RetriveAll.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Exceptions.RetriveAll.cs
@@ -43,6 +43,44 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Hosts
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+        [Fact]
+        public void ShouldThrowServiceExceptionOnRetriveAllIfServerErrorOccursAndLogItAsync()
+        {
+            //given
+            string exceptionMessage = GetRandomString();
+            var serverException = new Exception(exceptionMessage);
+
+            var failedHostServiceException = 
+                new FailedHostServiceException(serverException);
+            HostServiceAllException expectedHostServiceAllException =
+                new HostServiceAllException(failedHostServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllHosts()).Throws(serverException);
+
+            //when
+            Action retriveAllHostsAction = () =>
+                this.hostService.RetriveAllHosts();
+
+            HostServiceAllException actualHostServiceAllException = 
+                Assert.Throws<HostServiceAllException>(retriveAllHostsAction);
+
+            //then
+            actualHostServiceAllException.Should()
+                .BeEquivalentTo(expectedHostServiceAllException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllHosts(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedHostServiceAllException))),
+                Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+
+
 
         }
     }
