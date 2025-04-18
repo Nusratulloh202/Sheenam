142ea6e9f768ac6b059a1932b16b130a0786da0c
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-04-18 07:42:55 +0500

    CONTROLLER:PostHostAsync

--- Sheenam.Api/Services/Foundations/Hosts/IHostService.cs
+++ Sheenam.Api/Services/Foundations/Hosts/IHostService.cs
@@ -2,10 +2,15 @@
 // Copyright (c) Coalition of Good-Hearted Engineers
 // Free To Use To Find Comfort and Peace
 //==================================================
+using System.Linq;
+using System.Threading.Tasks;
+using Sheenam.Api.Models.Foundations.Hosts;
+
 namespace Sheenam.Api.Services.Foundations.Hosts
 {
     public interface IHostService
     {
-
+        ValueTask<Host> AddHostAsync(Host host);
+        IQueryable<Host> RetriveAllHosts();
     }
 }
