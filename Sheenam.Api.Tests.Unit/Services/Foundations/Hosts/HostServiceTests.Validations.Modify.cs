4877c7166c15788079c9a0ff556922bd3f88824d
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-05-15 10:28:11 +0500

    ShouldThrowValidationExceptionOnModifyIfHostIsInvalidAndLogITAsync->FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Validations.Modify.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Validations.Modify.cs
@@ -43,5 +43,59 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Hosts
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public async Task ShouldThrowValidationExceptionOnModifyIfHostIsInvalidAndLogITAsync(string invalidString)
+        {
+            //given
+            Host invalidHost = new Host
+            {
+                FirstName = invalidString
+            };
+            var invalidHostException = new InvalidHostException();
+            invalidHostException.AddData(
+                key:nameof(Host.Id),
+                values:  "Id is required" );
+            invalidHostException.AddData(
+                key: nameof(Host.FirstName),
+                values: "Text is required");
+            invalidHostException.AddData(
+                key: nameof(Host.LastName),
+                values: "Text is required");
+            invalidHostException.AddData(
+                key: nameof(Host.Email),
+                values: "Text is required");
+            invalidHostException.AddData(
+                key: nameof(Host.PhoneNumber),
+                values: "Text is required");
+            invalidHostException.AddData(
+                key: nameof(Host.DateOfBirth),
+                values: "Date is required");
+
+            var expectedHostValidationException = 
+                new HostValidationException(invalidHostException);
+            //when
+            ValueTask<Host> modifyHostTask = this.hostService.ModifyHostAsync(invalidHost);
+            var actualHostValidationException = await 
+                Assert.ThrowsAsync<HostValidationException>(modifyHostTask.AsTask);
+            //then
+            actualHostValidationException.Should()
+                .BeEquivalentTo(expectedHostValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(
+                SameExceptionAs(expectedHostValidationException))),
+                Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+            broker.UpdateHostAsync(It.IsAny<Host>()),
+                Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
