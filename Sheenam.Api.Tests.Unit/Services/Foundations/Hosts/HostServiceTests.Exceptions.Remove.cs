2ddc5fc0ee3a1b2c74c2ef19360cc059c4149442
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-05-17 01:16:55 +0500

    ShouldThrowServiceAllExceptionsOnRemoveIfServiceErrorOccursAndLogItAsync->FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Exceptions.Remove.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Exceptions.Remove.cs
@@ -97,5 +97,46 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Hosts
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
-    }
+
+        [Fact]
+        public async Task ShouldThrowServiceAllExceptionsOnRemoveIfServiceErrorOccursAndLogItAsync()
+        {
+            //given
+            Guid someHostId = Guid.NewGuid();
+
+            var serviceException = new Exception();
+
+            var failedHostServiceException =
+                new FailedHostServiceException(serviceException);
+            var expectedHostServiceException =
+                new HostServiceAllException(failedHostServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectByIdHostAsync(It.IsAny<Guid>()))
+                .ThrowsAsync(serviceException);
+            //when
+            ValueTask<Host> removeHostById =
+                this.hostService.RemoveHostAsync(someHostId);
+
+            var actualHostServiceException =
+                await Assert.ThrowsAsync<HostServiceAllException>(
+                    removeHostById.AsTask);
+            //then
+            actualHostServiceException.Should()
+                .BeEquivalentTo(expectedHostServiceException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedHostServiceException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectByIdHostAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteHostAsync(It.IsAny<Host>()), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }   
 }
