06c5ac67a51c94021aa0664c9a732acfed1c2a55
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-05-17 00:01:19 +0500

    ShouldRemoveHostAsync->Fail

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Logic.Remove.cs
@@ -0,0 +1,51 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Sheenam.Api.Models.Foundations.Hosts;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Hosts
+{
+    public partial class HostServiceTests
+    {
+        [Fact]
+        public async Task ShouldRemoveHostAsync()
+        {
+            //given
+            Host randomHost = CreateRandomHost();
+            Host inputHost = randomHost;
+            Host expectedRetrivedHostInput = inputHost;
+            Host deletedHost = expectedRetrivedHostInput;
+            Host expectedHost = deletedHost.DeepClone();
+            Guid randomHostId = Guid.NewGuid();
+            Guid inputHostId = randomHostId;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectByIdHostAsync(inputHostId))
+                    .ReturnsAsync(inputHost);
+            this.storageBrokerMock.Setup(broker =>
+                broker.DeleteHostAsync(inputHost))
+                    .ReturnsAsync(deletedHost);
+
+            //when
+            Host actualHost =
+                await this.hostService.RemoveHostAsync(inputHostId);
+            //then
+            actualHost.Should().BeEquivalentTo(expectedHost);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectByIdHostAsync(inputHostId),
+                Times.Once);
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteHostAsync(expectedRetrivedHostInput),
+                Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+
+        }
+    }
+}
