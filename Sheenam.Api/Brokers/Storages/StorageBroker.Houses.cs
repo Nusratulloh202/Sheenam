a6ae932a18198950e63dc9256fc6734c75fab015
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-05-17 14:48:56 +0500

    StorageBroker CRUD operations

--- Sheenam.Api/Brokers/Storages/StorageBroker.Houses.cs
+++ Sheenam.Api/Brokers/Storages/StorageBroker.Houses.cs
@@ -2,7 +2,11 @@
 // Copyright (c) Coalition of Good-Hearted Engineers
 // Free To Use To Find Comfort and Peace
 //==================================================
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Sheenam.Api.Models.Foundations.Guests;
 using Sheenam.Api.Models.Foundations.Home;
 
 namespace Sheenam.Api.Brokers.Storages
@@ -10,5 +14,18 @@ namespace Sheenam.Api.Brokers.Storages
     public partial class StorageBroker
     {
             public DbSet<Home> Home { get; set; }
+
+        public async ValueTask<Home> InsertHomeAsync(Home home)=>
+            await InsertAsync(home);
+        public IQueryable<Home> SelectAllHomes()=>
+            SelectAll<Home>();
+
+        public async ValueTask<Home> SelectHomeByIdAsync(Guid homeId) =>
+            await SelectAsync<Home>(homeId);
+
+        public async ValueTask<Home> UpdateHomeAsync(Home home) =>
+            await UpdateAsync(home);
+        public async ValueTask<Home> DeleteHomeAsync(Home home) =>
+            await DeleteAsync(home);
     }
 }
