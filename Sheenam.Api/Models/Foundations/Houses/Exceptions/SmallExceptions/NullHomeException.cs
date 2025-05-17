966e5de0189fc9266b357ac8008f536d1b3e7711
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-05-17 16:08:28 +0500

    NullHomeException

--- Sheenam.Api/Models/Foundations/Houses/Exceptions/SmallExceptions/NullHomeException.cs
+++ Sheenam.Api/Models/Foundations/Houses/Exceptions/SmallExceptions/NullHomeException.cs
@@ -1,6 +1,15 @@
-﻿namespace Sheenam.Api.Models.Foundations.Houses.Exceptions.SmallExceptions
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+using Xeptions;
+
+namespace Sheenam.Api.Models.Foundations.Houses.Exceptions.SmallExceptions
 {
-    public class NullHomeException
+    public class NullHomeException:Xeption
     {
+        public NullHomeException()
+            :base("Home is null. Try Again.")
+        {}
     }
 }
