281cf42c8d1d7c77e78f39ae93f59d2685ad6b1d
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-05-17 15:03:22 +0500

    IStorageBroker CRUD Update

---
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.Houese.cs
@@ -0,0 +1,20 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Sheenam.Api.Models.Foundations.Home;
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Home> InsertHomeAsync(Home home);
+        IQueryable<Home> SelectAllHomes();
+        ValueTask<Home> SelectHomeByIdAsync(Guid homeId);
+        ValueTask<Home> UpdateHomeAsync(Home home);
+        ValueTask<Home> DeleteHomeAsync(Home home);
+    }
+}
