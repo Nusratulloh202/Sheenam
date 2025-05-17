5b1b5143a3bb1db75d624275a0b051ae698b104d
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-05-17 15:47:02 +0500

    INITIALIZE: HomeServiceException

---
+++ Sheenam.Api/Models/Foundations/Houses/Exceptions/BigExceptions/HomeServiceException.cs
@@ -0,0 +1,15 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+using Xeptions;
+
+namespace Sheenam.Api.Models.Foundations.Houses.Exceptions.BigExceptions
+{
+    public class HomeServiceException:Xeption
+    {
+        public HomeServiceException(Xeption innerException)
+            : base(message: "Home service error occurred, contact support.", innerException)
+        {}
+    }
+}
