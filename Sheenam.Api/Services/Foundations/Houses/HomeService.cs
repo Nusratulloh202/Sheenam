b19053bb70f09d2881374df73e5fcad46e7cf4a7
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-05-17 15:33:03 +0500

    CODERUB:Code Update

--- Sheenam.Api/Services/Foundations/Houses/HomeService.cs
+++ Sheenam.Api/Services/Foundations/Houses/HomeService.cs
@@ -24,7 +24,6 @@ namespace Sheenam.Api.Services.Foundations.Houses
         public async ValueTask<Home> AddHomeAsync(Home home) =>
             await TryCatch(async () =>
             {
-                ValidateHome(home);
                 return await this.storageBroker.InsertHomeAsync(home);
             });
     }
