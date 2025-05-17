371b608ca3c0a6943f145828bd3649a88e8ab776
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-05-17 15:16:31 +0500

    IHomeService: CRUD

--- Sheenam.Api/Services/Foundations/Houses/IHomeService.cs
+++ Sheenam.Api/Services/Foundations/Houses/IHomeService.cs
@@ -2,9 +2,20 @@
 // Copyright (c) Coalition of Good-Hearted Engineers
 // Free To Use To Find Comfort and Peace
 //==================================================
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Sheenam.Api.Models.Foundations.Home;
+
 namespace Sheenam.Api.Services.Foundations.Houses
 {
     public interface IHomeService
     {
+        ValueTask<Home> AddHomeAsync(Home home);
+        IQueryable<Home> RetrieveAllHomes();
+        ValueTask<Home> RetrieveByIdHomeAsync(Guid homeId);
+        ValueTask<Home> ModifyHomeAsync(Home home);
+        ValueTask<Home> RemoveHomeAsync(Guid homeId);
+
     }
 }
