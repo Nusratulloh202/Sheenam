b105ba4ed05e86608242e4901c8eb61ee3672e84
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-05-17 16:00:01 +0500

    FailedHomeServiceException

--- Sheenam.Api/Models/Foundations/Houses/Exceptions/SmallExceptions/FailedHomeServiceException.cs
+++ Sheenam.Api/Models/Foundations/Houses/Exceptions/SmallExceptions/FailedHomeServiceException.cs
@@ -1,6 +1,18 @@
-﻿namespace Sheenam.Api.Models.Foundations.Houses.Exceptions.SmallExceptions
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+using System;
+using Xeptions;
+
+namespace Sheenam.Api.Models.Foundations.Houses.Exceptions.SmallExceptions
 {
-    public class FailedHomeServiceException
+    public class FailedHomeServiceException:Xeption
     {
+        public FailedHomeServiceException(Exception innerException)
+            :base(
+                message: "Failed home service error occurred, contact support.",
+                innerException)
+        { }
     }
 }
