c310097e3396121409a99663625cf21813e38aae
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-05-17 15:14:44 +0500

    Startup Update AddTransient HomeService

---
+++ Sheenam.Api/Services/Foundations/Houses/HomeService.cs
@@ -0,0 +1,15 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+using Sheenam.Api.Brokers.Logings;
+using Sheenam.Api.Brokers.Storages;
+
+namespace Sheenam.Api.Services.Foundations.Houses
+{
+    public class HomeService:IHomeService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+    }
+}
