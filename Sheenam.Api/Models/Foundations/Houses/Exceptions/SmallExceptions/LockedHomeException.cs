81a9f0fd25b3a126d33963354940281167c9565e
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-05-17 16:04:49 +0500

    LockedHomeException

--- Sheenam.Api/Models/Foundations/Houses/Exceptions/SmallExceptions/LockedHomeException.cs
+++ Sheenam.Api/Models/Foundations/Houses/Exceptions/SmallExceptions/LockedHomeException.cs
@@ -1,6 +1,16 @@
-﻿namespace Sheenam.Api.Models.Foundations.Houses.Exceptions.SmallExceptions
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+using System;
+using Xeptions;
+
+namespace Sheenam.Api.Models.Foundations.Houses.Exceptions.SmallExceptions
 {
-    public class LockedHomeException
+    public class LockedHomeException:Xeption
     {
+        public LockedHomeException(Exception innerException)
+            : base(message: "Home is locked, please unlock it to proceed.", innerException)
+        {}
     }
 }
